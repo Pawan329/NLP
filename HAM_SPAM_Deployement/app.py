ef0864e355c2b41020306343cc0849ab4b73bf73
Pawan Yadav <45333566+Pawan329@users.noreply.github.com>
2024-08-30 18:48:11 +0530

    Update app.py

--- HAM_SPAM_Deployement/app.py
+++ HAM_SPAM_Deployement/app.py
@@ -2,7 +2,7 @@ import pickle
 import streamlit as st
 
 #  Load the model from the pickle file (this is what you'd do in your Streamlit app)
-with open('spam_classifier.pkl', 'rb') as model_file:
+with open('models/spam_classifier.pkl', 'rb') as model_file:
     text_clf = pickle.load(model_file)
 
 # Streamlit App
